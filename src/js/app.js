5c1de1f4f7075139d9e29f0ef78a13b5132549f7
Dang Quoc Huy <huysamdua@yahoo.com>
2017-10-12 10:49:45 +0700

    Instantiating web3

--- src/js/app.js
+++ src/js/app.js
@@ -24,9 +24,14 @@ App = {
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    // Is there is an injected web3 instance?
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fallback to the TestRPC.
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
